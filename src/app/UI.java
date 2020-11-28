bcd25cd4e0732b1ed8f76896a1f09be9442c1ba4
Marcos Parrotto <marcosparrotto@gmail.com>
2020-11-28 11:31:43 -0300

    Cores nas peças

--- src/app/UI.java
+++ src/app/UI.java
@@ -1,8 +1,29 @@
 package app;
 
+import xadrez.Cor;
 import xadrez.PeçaXadrez;
 
 public class UI {
+	
+	public static final String ANSI_RESET = "\u001B[0m";
+	public static final String ANSI_BLACK = "\u001B[30m";
+	public static final String ANSI_RED = "\u001B[31m";
+	public static final String ANSI_GREEN = "\u001B[32m";
+	public static final String ANSI_YELLOW = "\u001B[33m";
+	public static final String ANSI_BLUE = "\u001B[34m";
+	public static final String ANSI_PURPLE = "\u001B[35m";
+	public static final String ANSI_CYAN = "\u001B[36m";
+	public static final String ANSI_WHITE = "\u001B[37m";
+
+	public static final String ANSI_BLACK_BACKGROUND = "\u001B[40m";
+	public static final String ANSI_RED_BACKGROUND = "\u001B[41m";
+	public static final String ANSI_GREEN_BACKGROUND = "\u001B[42m";
+	public static final String ANSI_YELLOW_BACKGROUND = "\u001B[43m";
+	public static final String ANSI_BLUE_BACKGROUND = "\u001B[44m";
+	public static final String ANSI_PURPLE_BACKGROUND = "\u001B[45m";
+	public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
+	public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
+	
 	public static void printTabuleiro(PeçaXadrez[][] peças) {
 		for(int i=0; i<peças.length;i++) {
 			System.out.print((8-i)+ " ");
@@ -15,11 +36,17 @@ public class UI {
 	}
 	
 	private static void printPeça(PeçaXadrez peça) {
-		if(peça == null) {
-			System.out.print("-");
-		} else {
-			System.out.print(peça);
-		}
-		System.out.print(" ");
+		if (peça == null) {
+            System.out.print("-");
+        }
+        else {
+            if (peça.getCor() == Cor.Brancas) {
+                System.out.print(ANSI_WHITE + peça + ANSI_RESET);
+            }
+            else {
+                System.out.print(ANSI_GREEN + peça + ANSI_RESET);
+            }
+        }
+        System.out.print(" ");
 	}
 }
